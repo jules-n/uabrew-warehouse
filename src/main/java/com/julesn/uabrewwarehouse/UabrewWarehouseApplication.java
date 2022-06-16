fbbb2330ba37a61cfe5780b1bde18a61c5b1e1c3
jules <neroba.yulia@gmail.com>
2022-06-16 10:01:46 +0300

    init commit

---
+++ src/main/java/com/julesn/uabrewwarehouse/UabrewWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.julesn.uabrewwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UabrewWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UabrewWarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julesn/uabrewwarehouse/UabrewWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julesn.uabrewwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UabrewWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
