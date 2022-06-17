582a095fe42deed4c68cc66bd01196f0227e3804
jules <neroba.yulia@gmail.com>
2022-06-17 20:53:12 +0300

    Added MenuController

---
+++ src/main/java/com/julesn/uabrewwarehouse/controllers/MenuController.java
@@ -0,0 +1,25 @@
+package com.julesn.uabrewwarehouse.controllers;
+
+import com.julesn.uabrewwarehouse.dtos.Menu;
+import com.julesn.uabrewwarehouse.services.MenuService;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/menu")
+public class MenuController {
+
+    @Setter(onMethod_ = {@Autowired})
+    private MenuService menuService;
+
+    @GetMapping("/{bar}/get")
+    public ResponseEntity<Menu> getMenu(@PathVariable String bar) {
+        var menu = menuService.getMenu(bar);
+        return menu.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.noContent().build());
+    }
+}
